84e0e26aa7d9d493e985977c2011ef13fe04baab
Dusan Antic <76735984+DusanAnticc@users.noreply.github.com>
2023-10-25 00:47:38 +0200

    Add files via upload

---
+++ TrendingConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using TrendingConsoleApp.ServiceReference2;
+
+namespace TrendingConsoleApp
+{
+    class Program
+    {
+        
+
+        public class Callback : ServiceReference2.ITrendingCallback
+        {
+            public void WriteToAlarmFile(string message)
+            {
+                Console.WriteLine($"[ALARM] Written \"{message}\" to file as alarming data");
+            }
+
+            public void WriteToConsole(string message)
+            {
+                Console.WriteLine($"[CONSOLE] Message arrived: {message}");
+            }
+
+            public void WriteToLogFile(string message)
+            {
+                Console.WriteLine($"[FILE] Written \"{message}\" to file as regular data");
+            }
+        }
+
+        static ServiceReference2.TrendingClient sc;
+        static void Main(string[] args)
+        {
+            InstanceContext ic = new InstanceContext(new Callback());
+            sc = new ServiceReference2.TrendingClient(ic);
+
+            sc.ConsoleTrendingInit();
+
+            Console.ReadKey();
+        }
+        
+    }
+}
